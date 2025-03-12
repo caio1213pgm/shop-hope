dc4cd87ad7baa08fcd18599dcba6f222d2e6b498
Caio1213 <caiofxpgm@gmail.com>
2025-03-12 17:32:00 -0300

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-300">
+        <h1 className="text-4xl text-white-500 ">Hello world</h1>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
