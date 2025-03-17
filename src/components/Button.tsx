2128a9b2188a5595aa3d88682e6b3770146b58bb
Caio1213 <caiofxpgm@gmail.com>
2025-03-17 17:28:50 -0300

    create button base

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from "react";
+
+type buttonProps = ComponentProps<"button">
+
+function Button({...props}: buttonProps) {
+    return (
+        <button {...props} className="bg-blue-500 text-white px-5 py-1.5 rounded cursor-pointer"/>
+    )
+}
+
+export default Button;
