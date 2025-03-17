2bbfc4c958b4f514d91e051a3417426b4efeaac2
Caio1213 <caiofxpgm@gmail.com>
2025-03-17 17:27:33 -0300

    add navlink in header and position of footer

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,14 +1,17 @@
 import { CircleUser } from "lucide-react";
 import { Search } from "lucide-react";
 import { ContainerSearch, InputSearch } from "../components/InputGroup";
+import { NavLink } from "react-router";
 
 function Header() {
   return (
     <header>
       <div className="w-full bg-blue-400 py-4 px-4 flex items-center justify-between">
-        <h2 className="text-white text-2xl font-medium hover:text-3xl transition-all duration-600 cursor-pointer">
-          SHOPHOPE
-        </h2>
+        <NavLink to="/">
+          <h2 className="text-white text-2xl font-medium hover:text-3xl transition-all duration-600 cursor-pointer">
+            SHOPHOPE
+          </h2>
+        </NavLink>
 
         <ContainerSearch>
           <InputSearch placeholder="Buscar na ShopHope" type="text" />
