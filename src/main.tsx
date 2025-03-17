478c9c25c61ce2e6f776621d403bbf7975f7e87d
Caio1213 <caiofxpgm@gmail.com>
2025-03-17 17:28:28 -0300

    add conection react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import PageProduct from "./pages/PageProduct.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/product" element={<PageProduct />} />
+    </Routes>
+  </BrowserRouter>
 );
