e4883abe9e5fd381e8e52b321a46cf0c0914f469
Caio1213 <caiofxpgm@gmail.com>
2025-03-20 16:52:30 -0300

    edit routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import PageProduct from "./pages/PageProduct.tsx";
+import PageCart from "./pages/PageCart.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/product" element={<PageProduct />} />
+      <Route path="/cart" element={<PageCart/>}/>
     </Routes>
   </BrowserRouter>
 );
