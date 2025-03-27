aa33c85c79cd02cc0c44c909e5bbaf9dba8267a9
Caio1213 <caiofxpgm@gmail.com>
2025-03-27 17:50:22 -0300

    fix useCartProvider - removeProduct working

--- src/context/cartContext.tsx
+++ src/context/cartContext.tsx
@@ -2,46 +2,47 @@ import { createContext, useContext, useState } from "react";
 import { PropsChildren } from "../layout/Design";
 
 export type itemProps = {
-	name: string | null;
-	price: string | null;
-	image: string | null;
-	description: string | null;
-	id: string | null;
+  name: string | null;
+  price: string | null;
+  image: string | null;
+  description: string | null;
+  id: string | null;
 };
 
 type propsContext = {
-	cart: itemProps[];
-	addProduct: (product: itemProps) => void;
-	removeProduct: (product: itemProps) => void;
+  cart: itemProps[];
+  addProduct: (product: itemProps) => void;
+  removeProduct: (product: itemProps) => void;
 };
 
 export const CartContext = createContext<propsContext>({
-	cart: [],
-	addProduct: () => {},
-	removeProduct: () => {},
+  cart: [],
+  addProduct: () => {},
+  removeProduct: () => {},
 });
 
 export default function CartProvider({ children }: PropsChildren) {
-	const [cart, setCart] = useState<itemProps[]>([]);
+  const [cart, setCart] = useState<itemProps[]>([]);
 
-	function addProduct(product: itemProps) {
-		setCart([...cart, product]);
-	}
+  function addProduct(product: itemProps) {
+    setCart([...cart, product]);
+  }
 
-	function removeProduct(product: itemProps) {
-		//Logica aqui
-		//const newCart: itemProps[] | undefined = cart?.filter(
-		//  (item) => item.id !== itemExluido.id
-		//);
-	}
+  function removeProduct(product: itemProps) {
+    //Logica aqui
+    const indexProduct = cart.findIndex((item) => item.id === product.id);
+    const newCart = [...cart];
+    newCart.splice(indexProduct, 1);
+    setCart(newCart);
+  }
 
-	return (
-		<CartContext.Provider value={{ cart, addProduct, removeProduct }}>
-			{children}
-		</CartContext.Provider>
-	);
+  return (
+    <CartContext.Provider value={{ cart, addProduct, removeProduct }}>
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export function useCartContext() {
-	return useContext(CartContext);
+  return useContext(CartContext);
 }
