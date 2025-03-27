e906d4193887fd7c1bf5854d4554cf983b8de503
Caio1213 <caiofxpgm@gmail.com>
2025-03-27 17:51:05 -0300

    edit bg-color

--- src/components/ContainerProducts.tsx
+++ src/components/ContainerProducts.tsx
@@ -2,7 +2,7 @@ import {PropsChildren} from "../layout/Design"
 
 function ContainerProducts({children}: PropsChildren){
     return (
-        <div className=" bg-blue-300 flex flex-wrap justify-center p-5 gap-y-3 gap-x-10 w-full">
+        <div className=" bg-blue-400 flex flex-wrap justify-center p-5 gap-y-3 gap-x-10 w-full">
             {children}
         </div>
     )
