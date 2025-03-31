56c3e7d1d16de230b828f9963661f13a741ee9e6
Caio1213 <caiofxpgm@gmail.com>
2025-03-31 15:55:53 -0300

    add loading itens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@
 import "../App.css";
 import ButtonCart from "../components/ButtonCart";
 import ContainerProducts from "../components/ContainerProducts";
-// import { ContainerSearch, InputSearch } from "../components/InputGroup";
 import Product, { productProps } from "../components/Product";
 import { useSearch } from "../context/searchContext";
 import Design from "../layout/Design";
@@ -10,20 +9,23 @@ import { useEffect, useState } from "react";
 
 function Home() {
   const [productsApi, setProducts] = useState<productProps[]>([]);
+  const [loading, setLoading] = useState(false);
 
-  const {search} = useSearch()
+  const { search } = useSearch();
 
   useEffect(() => {
+    setLoading(true);
     const fetchProducts = async () => {
       try {
         const response = await fetch("products.json");
         const data = await response.json();
         setProducts(data);
+        setLoading(false);
       } catch (error) {
         console.error("erro ao buscar produtos", error);
       }
     };
-    fetchProducts();
+    setTimeout(fetchProducts, 1000);
   }, []);
 
   const products = productsApi.filter((product) =>
@@ -33,18 +35,40 @@ function Home() {
   return (
     <>
       <Design>
-        <ContainerProducts>
-          {products.map((product) => (
-            <Product
-              key={product.id}
-              name={product.name}
-              price={product.price}
-              description={product.description}
-              image={product.image}
-              id={product.id}
-            />
-          ))}
-        </ContainerProducts>
+        {loading ? (
+          <ContainerProducts>
+            <span className="flex items-center gap-2 bg-blue-700 text-3xl text-white absolute p-2 rounded">
+              Carregando itens...
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-loader-circle-icon lucide-loader-circle animate-spin"
+              >
+                <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+              </svg>
+            </span>
+          </ContainerProducts>
+        ) : (
+          <ContainerProducts>
+            {products.map((product) => (
+              <Product
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                description={product.description}
+                image={product.image}
+                id={product.id}
+              />
+            ))}
+          </ContainerProducts>
+        )}
+
         <ButtonCart />
       </Design>
     </>
