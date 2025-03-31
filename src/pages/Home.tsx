ff4af692c1544a6e01b05a9a50202ff946db8294
Caio1213 <caiofxpgm@gmail.com>
2025-03-31 16:06:56 -0300

    create component for loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,29 @@ import { useSearch } from "../context/searchContext";
 import Design from "../layout/Design";
 import { useEffect, useState } from "react";
 
+function LoadingItems() {
+  return (
+    <ContainerProducts>
+      <span className="flex items-center gap-2 bg-blue-700 text-3xl text-white absolute p-2 rounded">
+        Carregando itens...
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-loader-circle-icon lucide-loader-circle animate-spin"
+        >
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+      </span>
+    </ContainerProducts>
+  );
+}
+
 function Home() {
   const [productsApi, setProducts] = useState<productProps[]>([]);
   const [loading, setLoading] = useState(false);
@@ -36,24 +59,7 @@ function Home() {
     <>
       <Design>
         {loading ? (
-          <ContainerProducts>
-            <span className="flex items-center gap-2 bg-blue-700 text-3xl text-white absolute p-2 rounded">
-              Carregando itens...
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-loader-circle-icon lucide-loader-circle animate-spin"
-              >
-                <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-              </svg>
-            </span>
-          </ContainerProducts>
+          <LoadingItems/>
         ) : (
           <ContainerProducts>
             {products.map((product) => (
