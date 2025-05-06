794de4872b04091a47acddc110c7852f3d75f7fc
Caio1213 <caiofxpgm@gmail.com>
2025-05-06 18:06:43 -0300

    refactor: login method, add react-hook-forms

--- src/components/InputGroupLogin.tsx
+++ src/components/InputGroupLogin.tsx
@@ -3,11 +3,11 @@ import { ComponentProps } from "react";
 type divProps = ComponentProps<"div">;
 export function ContainerLogin({...props}: divProps){
     return(
-        <div {...props} className="flex px-2 py-3 w-full focus-within:w-full focus-within:border-white-600 bg-white border-3 border-white-200 transition-all duration-650 rounded-2xl mt-1"/>
+        <div {...props} className="flex px-2 py-3 w-full focus-within:w-full focus-within:border-white-600 bg-white border-3 border-white-200 transition-all duration-650 rounded-2xl mt-1 "/>
     )
 }
 
 type inputProps = ComponentProps<"input">;
 export function InputLogin({ ...props }: inputProps) {
-  return <input {...props} required className="w-full outline-0" />;
+  return <input {...props} className="w-full outline-0" />;
 }
